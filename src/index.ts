2f82ac8485803884cb27c4895385eb4f1edfc0ee
Satoshi Takeda <tkdnation@gmail.com>
2019-08-27 02:24:41 +0900

    add: source files

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-console.log("hoge");
+import { add, subtract } from "./libs/modules";
+
+console.log(`add: 1 + 2 = ${add(1, 2)}`);
+console.log(`subtract: 4 - 1 = ${subtract(4, 1)}`);
---
+++ test/modules.test.ts
@@ -0,0 +1,3 @@
+import assert from "assert";
+import * as mod from "../src/libs/modules";
+import sinon from "sinon";
