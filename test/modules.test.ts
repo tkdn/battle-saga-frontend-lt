ca371676c06610a296f125baefaa18b1da50cb16
tkdn <tkdnation@gmail.com>
2019-08-27 12:20:01 +0900

    update: test

--- test/modules.test.ts
+++ test/modules.test.ts
@@ -1,3 +1,17 @@
+import sinon from "sinon";
 import assert from "assert";
 import * as mod from "../src/libs/modules";
-import sinon from "sinon";
+
+const fooSpy = sinon.spy(mod["foo"]);
+const getterStub = sinon.stub(mod, "foo").get(() => fooSpy);
+
+mod.foo();
+
+try {
+    assert(fooSpy.called);
+    console.log("passed");
+} catch (e) {
+    console.error(e);
+}
+
+getterStub.restore();
